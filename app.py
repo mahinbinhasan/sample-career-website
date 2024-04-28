d575b373cb5c7af586f790ec38c769416cb868b4
Mahin Bin Hasan <80515603+mahinbinhasan@users.noreply.github.com>
2024-04-28 04:34:08 +0000

    DD

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
+JOBS=[
+
+  {
+    'id': 1,
+    'title':'Data Analyst',
+    'location':'Dhaka',
+    'salary':'BDT.15,000',
+  },
+  {
+    'id': 2,
+    'title':'CTO',
+    'location':'Dhaka',
+    'salary':'BDT.70,000',
+  }
+]
 @app.route('/')
 def hello_world():
-  return "hello, world"
+  return render_template('home.html', jobs= JOBS)
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == '__main__':
   app.run(host="0.0.0.0", debug=True)
